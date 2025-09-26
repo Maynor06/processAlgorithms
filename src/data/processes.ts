71310995d5b951f2ca77c5d3f0ed509e73b5e9e4
ANDERSITOGT <lopezanderson4182@gmail.com>
2025-09-26 13:24:11 -0600

    Update processes.ts

--- src/data/processes.ts
+++ src/data/processes.ts
@@ -3,8 +3,8 @@ import type { Process } from "../Components/Algorithms/common";
 // Dataset inicial (puedes tener más presets después)
 export const initialProcesses: Process[] = [
 { pid: 1, name: "A", arrivalTime: 0, burstTime: 5 },  // largo, llega primero
-{ pid: 2, name: "H", arrivalTime: 3, burstTime: 1 },   // cortos que llegan después
-{ pid: 3, name: "B", arrivalTime: 3, burstTime: 1 },
+{ pid: 2, name: "B", arrivalTime: 3, burstTime: 1 },   // cortos que llegan después
+{ pid: 3, name: "H", arrivalTime: 3, burstTime: 1 },
 { pid: 4, name: "E", arrivalTime: 10, burstTime: 3 },
 { pid: 5, name: "C", arrivalTime: 11, burstTime: 1 },
 { pid: 6, name: "F", arrivalTime: 12, burstTime: 1 },
